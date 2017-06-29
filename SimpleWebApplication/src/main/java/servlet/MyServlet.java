bb0104b7b158505d07ab219cbc522e1eb93f9527
vikb717 <vikb717@gmail.com>
2017-06-29 19:50:18 +0300

    added simple servlet

---
+++ SimpleWebApplication/src/main/java/servlet/MyServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by viktoryiabolbas on 6/28/17.
+ */
+@WebServlet("/s")
+public class MyServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("Hello World!");
+    }
+}
